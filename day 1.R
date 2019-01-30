bef4b9672c21274184e4edbe57b77d89a804f8b7
vhuawelo <simba.vhuawelo@gmail.com>
2019-01-30 08:44:49 +0200

    first commit

---
+++ day 1.R
@@ -0,0 +1,20 @@
+#DAY_1.R
+#Calculate a monthly climatology per site
+#Author: Nobuhle and vhuawelo
+#Date:29 january 2019
+
+library(tidyverse)
+library(lubridate)
+
+temp <- read_csv("SACTN_data.csv")
+
+temp2 <- temp%>%
+dplyr::mutate(month=month(date))%>%
+dplyr::group_by(site,month) %>% 
+dplyr::summarise(temp=mean(temp,na.rm = true)) %>% 
+ungroup()
+
+ggplot(data=temp2, aes(x=month,y=temp))+
+  geom_line()+
+  facet_wrap(-site,nrow=3)+
+  labs(x="month",y="temperature(")
---
+++ day1_laminaria.R
@@ -0,0 +1,92 @@
+#Day1
+#laminaria dataset exploring and learning
+#Vhuawelo/Nobuhle
+#29 jan 2019
+
+#loading libraries
+
+library(tidyverse)
+lam <- read_csv("laminaria.csv")
+head(lam)#shows first six rows
+tail(lam)
+head(lam,n=3)#To show the fisrt 3 rows
+tail(lam,n=3)
+
+lam_select <- lam %>% 
+  select(site,total_length) %>% 
+slice(54:80)
+
+lam_kom <- lam %>% 
+  filter(site=="Kommetjie")
+
+#R is case sensitive,stick to one case
+#in the laminaria dataset select only the site and the blade_length column
+#filter only the Sea Point
+
+lam_try <- lam %>% 
+select(site, blade_length) %>% 
+filter(site=="Sea Point")
+
+lam %>% 
+  filter(total_length==max(total_length))
+
+summary(lam)
+dim(lam)
+
+lam %>% 
+  summarize(avrg_bl=mean(blade_length),
+            med_bl=median(blade_length),
+            sd_bl=sd(blade_length)) 
+#don't use %>% beacuse it is the same function, instead use comma.
+
+lam %>% 
+  group_by(site) %>% 
+  summarise(var_bl=var(blade_length),
+            n=n()) %>% 
+  mutate(se=sqrt(var_bl/n))
+#mutate create a new column
+
+lam_2 <- lam %>% 
+  select(-blade_thickness, -blade_length)
+
+lam %>%
+  select(blade_length) %>%
+  na.omit %>% 
+  summarise(n=n())
+
+ lam %>%
+  select(stipe_mass) %>%
+   na.omit %>% 
+  summarise(n=n())
+# na.omit excludes the columns with N/A meaning those that are empty.
+ 
+ ggplot(data = lam, aes(x = stipe_mass, y = stipe_length)) +
+   geom_point(shape = 21, colour = "blue", fill = "red") +
+   labs(x = "Stipe mass (kg)", y = "Stipe length (cm)")
+# Drawing graphs
+ 
+ lam_exe <- lam %>% 
+   select(site) %>% 
+   mutate(total_length_half=total_length/2)
+ na.omit %>%
+   
+ #Create a new data frame from the `laminaria` dataset that meets the following criteria: contains only the `site` column and a new column called `total_length_half` containing values that are half of the `total_length`. In this `total_length_half` column, there are no `NA`s and all values are less than 100.
+ #think about how the commands should be ordered to produce this data frame!
+ 
+ 
+ lam %>% 
+   group_by(site) %>%
+   summarise(avr_bl=mean(blade_length),
+             min_bl=min(blade_length),
+             max_bl=max(blade_length),
+             n=n())
+ #Use `group_by()` and `summarize()` to find the mean, min, and max blade_length for each site. Also add the number of observations (hint: see `?n`).
+
+ lam %>%
+   select(stipe_mass) %>% 
+  filter(stipe_mass==max(stipe_mass))
+
+  #What was the heaviest stipe measured in each site? Return the columns `site`, `region`, and `stipe_length`
+ 
+  
+ 
